256007210578be8e6971105f8a1f2a5e1363f1b9
ustargab@gmail.com <OHvtMh80>
2018-08-14 13:39:00 +0200

    First tour of heroes

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice', status: 'Online'},
+  { id: 12, name: 'Narco', status: 'Online' },
+  { id: 13, name: 'Bombasto', status: 'Online' },
+  { id: 14, name: 'Celeritas', status: 'Online' },
+  { id: 15, name: 'Magneta', status: 'Online' },
+  { id: 16, name: 'RubberMan', status: 'Offline' },
+  { id: 17, name: 'Dynama', status: 'Online' },
+  { id: 18, name: 'Dr IQ', status: 'Online' },
+  { id: 19, name: 'Magma', status: 'Offline' },
+  { id: 20, name: 'Tornado', status: 'Online' }
+];
